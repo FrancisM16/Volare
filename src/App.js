3dad3549235bc22e838da651a32c01d92ade4481
Francis Mori <mfrancis794@gmail.com>
2023-02-19 01:52:08 -0500

    Add ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <ItemListContainer greeting="¡Bienvenidos todos!"/>
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,7 @@
-export const ItemListContainer = ({greeting}) => {
-    return(
-        <article>
-            <h2> {greeting} <span className="text-indigo-500">usuarios</span></h2>
+export const ItemListContainer = ({ greeting }) => {
+    return (
+        <article className="container mx-auto mt-4 font-default text-xl">
+            <h1> {greeting} <span className="text-indigo-500">a Volare</span></h1>
         </article>
     )
 }
