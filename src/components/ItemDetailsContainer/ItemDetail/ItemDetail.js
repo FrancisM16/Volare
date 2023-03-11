f6cd147979fde1ff873b76d926b0f076978ff8b2
Francis Mori <mfrancis794@gmail.com>
2023-03-10 21:28:51 -0500

    change screen breakpoint

--- src/components/ItemDetailsContainer/ItemDetail/ItemDetail.js
+++ src/components/ItemDetailsContainer/ItemDetail/ItemDetail.js
@@ -11,14 +11,14 @@ export const ItemDetail = ({ item }) => {
     }
 
     return (
-        <div className="container mx-auto md:py-6 space-y-6 font-default px-4 py-4">
+        <div className="container mx-auto md:py-6 space-y-6 font-default p-4">
             <button onClick={handleVolver} className="hover:text-indigo-600 space-x-2">
                 <FontAwesomeIcon icon={solid('chevron-left')} size='sm' />
                 <span>Volver</span>
             </button>
-            <div className="md:flex md:items-center border border-slate-200 bg-white rounded-lg shadow-sm flex-none p-4 md:space-x-8 space-y-8">
+            <div className="md:flex md:items-center border border-slate-200 bg-white rounded-lg shadow-sm flex-none p-4 md:space-x-8 space-y-8 md:space-y-0">
                 <img className="md:object-scale-down md:h-1/5 md:w-1/5 h-full w-full rounded-md" src={item.pictureUrl} alt={item.title} />
-                <div className="space-y-4">
+                <div className="space-y-4 md:space-y-0">
                     <div>
                         <h2 className="text-2xl font-semibold">{item.title}</h2>
                         <p className="text-xl text-indigo-900">${item.price}</p>
--- src/components/ItemDetailsContainer/ItemDetailsContainer.js
+++ src/components/ItemDetailsContainer/ItemDetailsContainer.js
@@ -20,6 +20,7 @@ export const ItemDetailsContainer = () => {
             .finally(() => {
                 setLoading(false)
             })
+    // eslint-disable-next-line 
     }, [])
 
     return (
