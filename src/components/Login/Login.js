bcae866ed4e98a73c064b758ed44cb45b0eefc28
Francis Mori <mfrancis794@gmail.com>
2023-04-04 06:05:31 -0500

    adjust login and register

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,69 +1,84 @@
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from "../../context/AuthContext"
 import { CartContext } from '../../context/CartContext'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro"
+import { useForm } from 'react-hook-form'
+import bags from "../../assets/bags.svg";
 
 export const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm()
     const { login } = useContext(AuthContext)
     const { cart } = useContext(CartContext)
     const navigate = useNavigate()
 
-    const [values, setValues] = useState({
-        email: '',
-        password: ''
-    })
-
-    const handleInputChange = (e) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        login(values)
+    const onSubmit = (data) => {
+        login(data)
         cart.length === 0 ? navigate("/") : navigate("/cart")
     }
 
     return (
-        <div className="container mx-auto">
-            <div className="py-4 px-4 md:px-0 space-y-8">
-                <h2 className="text-xl">Iniciar sesión</h2>
-                <form className="space-y-6" onSubmit={handleSubmit}>
-                    <label className="block">
-                        <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                            Email
-                        </span>
-                        <input
-                            type={"email"}
-                            onChange={handleInputChange}
-                            value={values.email}
-                            placeholder="Ej. daniela@gmail.com"
-                            name="email"
-                            className="placeholder-slate-400 sm:text-sm focus:ring-1"
-                        />
-                    </label>
+        <div className="flex w-full h-screen">
+            <div className="w-full flex items-center justify-center lg:w-1/2">
+                <div className="w-11/12 max-w-[700px] px-10 py-20 rounded-3xl bg-white border-2 border-gray-100">
+                    <h2 className="text-xl">Iniciar sesión</h2>
+                    <div className="mt-4">
+                        <form className="space-y-6 " onSubmit={handleSubmit(onSubmit)}>
+                            <label className="flex flex-col space-y-2">
+                                <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                                    Correo electrónico
+                                </span>
+                                <input
+                                    type={"email"}
+                                    placeholder="Ej. daniela@gmail.com"
+                                    className="placeholder-slate-400 sm:text-sm focus:ring-1"
+                                    {...register("email", { required: true })}
+                                />
+                                {errors.email &&
+                                    <div className="space-x-2 text-red-600">
+                                        <FontAwesomeIcon icon={solid('circle-exclamation')} size='xs' />
+                                        <span className="text-sm">Correo electrónico es obligatorio</span>
+                                    </div>}
+                            </label>
 
-                    <label className="block">
-                        <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                            Contraseña
-                        </span>
-                        <input
-                            type={"password"}
-                            onChange={handleInputChange}
-                            value={values.password}
-                            placeholder="Ej. ********"
-                            name="password"
-                            className="placeholder-slate-400 sm:text-sm focus:ring-1"
-                        />
-                    </label>
+                            <label className="flex flex-col space-y-2">
+                                <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                                    Contraseña
+                                </span>
+                                <input
+                                    type={"password"}
+                                    placeholder="Ej. ********"
+                                    className="placeholder-slate-400 sm:text-sm focus:ring-1"
+                                    {...register("password", { required: true, minLength: 8 })}
+                                />
+                                {errors.password && errors.password.type !== "minLength" &&
+                                    <div className="space-x-2 text-red-600">
+                                        <FontAwesomeIcon icon={solid('circle-exclamation')} size='xs' />
+                                        <span className="text-sm">Contraseña es obligatoria</span>
+                                    </div>}
+                                {errors.password && errors.password.type === "minLength" &&
+                                    <div className="space-x-2 text-red-600">
+                                        <FontAwesomeIcon icon={solid('circle-exclamation')} size='xs' />
+                                        <span className="text-sm">Se necesita mínimo 8 caracteres</span>
+                                    </div>
+                                }
+                            </label>
 
-                    <div className='space-x-8'>
-                        <button className='btn btn-primary' type='submit'>Login</button>
-                        <Link to="/register">Registrarme</Link>
+                            <div className="grid justify-items-center space-x-0 space-y-6 md:flex md:items-center md:space-y-0 md:space-x-4">
+                                <button className="border rounded-md py-2 px-4 text-white bg-violet-900 hover:bg-black md:w-fit w-full" type="submit">Iniciar sesión</button>
+                                <div className="text-center">
+                                    <Link to="/register" className=" hover:text-violet-900">Registrarme</Link>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
+
+            </div>
+            <div className="hidden relative w-1/2 h-full lg:flex items-center justify-center bg-amber-50">
+                <div className="w-80 h-80 rounded-full bg-gradient-to-tr from-violet-900 to-orange-200 animate-[pulse_8s_ease-in-out_infinite]" />
+                <img src={bags} alt="logo" className="absolute" />
             </div>
         </div>
     )
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,101 +1,119 @@
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../context/AuthContext"
 import { CartContext } from "../../context/CartContext"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro"
+import { useForm } from 'react-hook-form'
+import cartShop from "../../assets/cartShop.svg";
 
 export const Register = () => {
-    
+    const { register, formState: { errors }, handleSubmit } = useForm()
     const { registerUser } = useContext(AuthContext)
     const { cart } = useContext(CartContext)
     const navigate = useNavigate()
 
-    const [values, setValues] = useState({
-        name: '',
-        address: '',
-        email: '',
-        password: ''
-    })
-
-    const handleInputChange = (e) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        registerUser(values)
+    const onSubmit = (data) => {
+        registerUser(data)
         cart.length === 0 ? navigate("/") : navigate("/cart")
     }
 
     return (
-        <div className="container mx-auto">
-            <div className="py-4 px-4 md:px-0 space-y-8">
-                <h2 className="text-xl">Registrarse</h2>
-                <form className="space-y-6" onSubmit={handleSubmit}>
-                    <label className="block">
-                        <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                            Nombre
-                        </span>
-                        <input
-                            type={"text"}
-                            onChange={handleInputChange}
-                            value={values.name}
-                            placeholder="Ej. Daniela Gonzales"
-                            name="name"
-                            className="placeholder-slate-400 sm:text-sm focus:ring-1"
-                        />
-                    </label>
+        <div className="flex w-full h-screen">
+            <div className="w-full flex items-center justify-center lg:w-1/2">
+                <div className="w-11/12 max-w-[700px] px-10 py-20 rounded-3xl bg-white border-2 border-gray-100">
+                    <h2 className="text-xl">Registrarse</h2>
+                    <div className="mt-4">
+                        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                            <label className="block space-y-2">
+                                <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                                    Nombre
+                                </span>
+                                <input
+                                    type={"text"}
+                                    placeholder="Ej. Daniela Gonzales"
+                                    className="placeholder-slate-400 sm:text-sm focus:ring-1"
+                                    {...register("name", { required: true })}
+                                />
+                                {errors.name &&
+                                    <div className="space-x-2 text-red-600">
+                                        <FontAwesomeIcon icon={solid('circle-exclamation')} size='xs' />
+                                        <span className="text-sm">Nombre es obligatorio</span>
+                                    </div>}
 
-                    <label className="block">
-                        <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                            Dirección
-                        </span>
-                        <input
-                            type={"text"}
-                            onChange={handleInputChange}
-                            value={values.address}
-                            placeholder="Ej. Av. República de Panamá 153"
-                            name="address"
-                            className="placeholder-slate-400 sm:text-sm focus:ring-1"
-                        />
-                    </label>
+                            </label>
 
-                    <label className="block">
-                        <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                            Email
-                        </span>
-                        <input
-                            type={"email"}
-                            onChange={handleInputChange}
-                            value={values.email}
-                            placeholder="Ej. daniela@gmail.com"
-                            name="email"
-                            className="placeholder-slate-400 sm:text-sm focus:ring-1"
-                        />
-                    </label>
+                            <label className="block space-y-2">
+                                <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                                    Dirección
+                                </span>
+                                <input
+                                    type={"text"}
+                                    placeholder="Ej. Av. República de Panamá 153"
+                                    className="placeholder-slate-400 sm:text-sm focus:ring-1"
+                                    {...register("address", { required: true })}
+                                />
+                                {errors.address &&
+                                    <div className="space-x-2 text-red-600">
+                                        <FontAwesomeIcon icon={solid('circle-exclamation')} size='xs' />
+                                        <span className="text-sm">Dirección es obligatoria</span>
+                                    </div>}
+                            </label>
 
-                    <label className="block">
-                        <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                            Contraseña
-                        </span>
-                        <input
-                            type={"password"}
-                            onChange={handleInputChange}
-                            value={values.password}
-                            placeholder="Ej. ********"
-                            name="password"
-                            className="placeholder-slate-400 sm:text-sm focus:ring-1"
-                        />
-                    </label>
-                    <div className='space-x-8'>
-                        <button className='btn btn-primary' type='submit'>Crear usuario</button>
-                        <Link to="/login">Ya estoy registrado, logearme</Link>
+                            <label className="block space-y-2">
+                                <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                                    Correo electrónico
+                                </span>
+                                <input
+                                    type={"email"}
+                                    placeholder="Ej. daniela@gmail.com"
+                                    className="placeholder-slate-400 sm:text-sm focus:ring-1"
+                                    {...register("email", { required: true })}
+                                />
+                                {errors.email &&
+                                    <div className="space-x-2 text-red-600">
+                                        <FontAwesomeIcon icon={solid('circle-exclamation')} size='xs' />
+                                        <span className="text-sm">Correo electrónico es obligatorio</span>
+                                    </div>}
+                            </label>
+
+                            <label className="block space-y-2">
+                                <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                                    Contraseña
+                                </span>
+                                <input
+                                    type={"password"}
+                                    placeholder="Ej. ********"
+                                    className="placeholder-slate-400 sm:text-sm focus:ring-1"
+                                    {...register("password", { required: true, minLength: 8 })}
+                                />
+                                {errors.password && errors.password.type !== "minLength" &&
+                                    <div className="space-x-2 text-red-600">
+                                        <FontAwesomeIcon icon={solid('circle-exclamation')} size='xs' />
+                                        <span className="text-sm">Contraseña es obligatoria</span>
+                                    </div>}
+                                {errors.password && errors.password.type === "minLength" &&
+                                    <div className="space-x-2 text-red-600">
+                                        <FontAwesomeIcon icon={solid('circle-exclamation')} size='xs' />
+                                        <span className="text-sm">Se necesita mínimo 8 caracteres</span>
+                                    </div>
+                                }
+                            </label>
+                            <div className="grid justify-items-center space-x-0 space-y-6 md:flex md:items-center md:space-y-0 md:space-x-4">
+                                <button className="border rounded-md py-2 px-4 text-white bg-violet-900 hover:bg-black md:w-fit w-full" type='submit'>Crear usuario</button>
+                                <div className="text-center">
+                                    <Link to="/login" className=" hover:text-violet-900 m-4">Ya tengo cuenta, iniciar sesión</Link>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                </form>
-            </div>
+                </div>
 
+            </div>
+            <div className="hidden relative w-1/2 h-full lg:flex items-center justify-center bg-amber-50">
+                <div className="w-80 h-80 rounded-full bg-gradient-to-tr from-violet-300 to-orange-200 animate-[pulse_8s_ease-in-out_infinite]" />
+                <img src={cartShop} alt="logo" className="absolute" />
+            </div>
         </div>
     )
 }
